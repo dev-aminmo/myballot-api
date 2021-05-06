8fb56ae8be8ba1feef133cb9abec222d778fd249
Mostefaoui-Mohammed-Amin <mohammed.amin.mostefaoui@gmail.com>
2021-05-06 01:25:02 +0100

    create elections table

---
+++ database/migrations/2021_05_06_000528_create_elections_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateElectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('elections', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->bigInteger('admin_id')->unsigned();
+            $table->foreign("admin_id")->references('id')->on("users");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('elections');
+    }
+}
