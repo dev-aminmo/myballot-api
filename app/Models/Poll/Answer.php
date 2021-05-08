fe375778ab77fa372ca3c6f73ef56d56d6e8f3f7
Mostefaoui-Mohammed-Amin <mohammed.amin.mostefaoui@gmail.com>
2021-05-09 00:36:27 +0100

    creating answer,question models

---
+++ app/Models/Poll/Answer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Answer extends Model
+{
+    use HasFactory;
+    protected $fillable=['value','question_id'];
+    public $timestamps=false;
+
+}
