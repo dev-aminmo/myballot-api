b198dbd9683ad406896ab4a14f6397219b2a89ef
Mostefaoui-Mohammed-Amin <mohammed.amin.mostefaoui@gmail.com>
2021-05-23 22:58:48 +0100

    adding command to automatically notify users

---
+++ app/Console/Commands/NotifyUsers.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\SendMailsJob;
+use App\Models\Election;
+use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+
+class NotifyUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'notify:users';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send an email to users';
+
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+        $now = date("Y-m-d H:i:00", strtotime(Carbon::now()));
+        logger($now);
+
+        $elections_starts = Election::where("start_date", $now)->get();
+        $elections_ends = Election::where("end_date", $now)->get();
+        if (!$elections_starts->isEmpty()) {
+            foreach ($elections_starts as $election) {
+                $election->users()->where('election_id', $election->id)->get()->each(function ($user) {
+                    $data = ['type' => 3, "email" => $user->email];
+                    dispatch(new SendMailsJob($data));
+                    return $user;
+                });
+            }
+
+        }
+    }
+}
