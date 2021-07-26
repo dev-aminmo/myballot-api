498739ce81cba19af3b1203e7ec720a8fbba8f96
Mostefaoui-Mohammed-Amin <mohammed.amin.mostefaoui@gmail.com>
2021-07-26 21:00:54 +0100

    create plurality request refactor

--- app/Http/Requests/PluralityElection/CreatePluralityElectionRequest.php
+++ app/Http/Requests/PluralityElection/CreatePluralityElectionRequest.php
@@ -35,16 +35,10 @@ class CreatePluralityElectionRequest extends FormRequest
             'end_date'      => 'required|date|date_format:Y-m-d H:i|after:start_date',
             'title'=> 'required|string|min:2|max:255',
             'description'=> 'string|min:10|max:400',
-            'candidate_type'=>'required|integer|min:0|max:1',
             'seats_number'=>'integer|min:1',
-            'parties' => 'array|min:1|max:30',
-            'parties.*.name'=>'required',
-            'parties.*.candidates' => 'required|array|min:1|max:1',
-            'parties.*.candidates.*.name' => 'required|string|min:4|max:255',
-            'parties.*.candidates.*.description' => 'string|min:4|max:400',
-            'free_candidates'=>'array|min:1|max:30',
-            'free_candidates.*.name' => 'required|string|min:4|max:255',
-            'free_candidates.*.description' => 'string|min:4|max:400',
+            'candidates'=>'array|min:1|max:30',
+            'candidates.*.name' => 'required|string|min:4|max:255',
+            'candidates.*.description' => 'string|min:4|max:400',
         ];
     }
 
@@ -57,7 +51,6 @@ class CreatePluralityElectionRequest extends FormRequest
     {
         return $this->validator->validated();
     }
-
     /**
      * this method check that there is more than two candidates and the difference
      * between start_date and end_date is more than 5 minutes
@@ -68,11 +61,6 @@ class CreatePluralityElectionRequest extends FormRequest
         $candidates_count=0;
         if (!empty($this->free_candidates)) {
             $candidates_count +=count($this->free_candidates);
-        }
-        if (!empty($this->parties) && !empty($this->type) == 1) {
-            foreach($this->parties as $party){
-                $candidates_count += count(  $party['candidates']);
-            }
         }
         if ($candidates_count<2) {
             throw new HttpResponseException( $this->returnValidationResponse(["the minimum number of candidates is 2"]));
